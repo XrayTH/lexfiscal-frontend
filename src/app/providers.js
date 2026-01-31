c14b9ad636e5a8f3883067814cde8a0070a290c0
XrayTH <143976649+XrayTH@users.noreply.github.com>
2026-01-31 15:19:31 -0500

    fix: hydration mismatch fix

--- src/app/providers.js
+++ src/app/providers.js
@@ -1,13 +1,18 @@
 "use client"
 
 import { ChakraProvider } from "@chakra-ui/react"
+import { CacheProvider } from "@emotion/react"
+import emotionCache from "@/theme/emotionCache"
 import { system } from "@/theme/theme"
 
 export default function Providers({children}){
   return(
-    <ChakraProvider value={system}>
-      {children}
-    </ChakraProvider>
+    <CacheProvider value={emotionCache}>
+      <ChakraProvider value={system}>
+        {children}
+      </ChakraProvider>
+    </CacheProvider>
   )
 }
+
 
